43c99330ffd8231d96d5f891ef91c2b69cbcbbba
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-11-29 08:55:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import streamlit as st
-st.header('hola!')
+
+st.title('hola!')
+
+data_file = st.file_uploader('choose file', type=([.csv]))
+
+if data_file is not None:
+  df = pd.read_csv(data_file)
+
+  st.dataframe(df)
