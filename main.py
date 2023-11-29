e16e43309c51cf5cbb7ebc28b6622f627afdc7ef
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-11-29 09:25:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import io
 st.title('hola!')
 
 st.header('up file')
-data_file = st.file_uploader('choose file', type=([.csv]))
+data_file = st.file_uploader('choose file', type=(['.csv']))
 
 if data_file is not None:
   df = pd.read_csv(data_file)
