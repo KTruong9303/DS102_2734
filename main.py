09d9b32ed6b722887198176e7c193a8e17b5d5c4
Vũ Kiên Trường <91974329+KTruong9303@users.noreply.github.com>
2023-11-29 09:05:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 st.title('hola!')
 
+st.header('up file')
 data_file = st.file_uploader('choose file', type=([.csv]))
 
 if data_file is not None:
   df = pd.read_csv(data_file)
-
+  st.header('show data')
   st.dataframe(df)
+  st.header('statistic:')
+  st.table(df.describe())
